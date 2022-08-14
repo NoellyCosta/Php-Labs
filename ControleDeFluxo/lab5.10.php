ce235452d85c2e8560f6f890614af93b1e47c3ae
noelly1232 <59965675+NoellyCosta@users.noreply.github.com>
2022-08-14 21:11:00 +0000

    finalizando controle de fluxo

---
+++ ControleDeFluxo/lab5.10.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <title>Loops com incrementos diferentes</title>
+  </head>
+
+  <body>
+
+
+    <?php 
+for($x = -5; $x <= 15; $x = $x + 2){
+  print "O valor da variável $x é:  $x <br />";
+}
+?>
+  </body>
+</html>
---
+++ ControleDeFluxo/lab5.11.php
@@ -0,0 +1,36 @@
+<html>
+  <head>
+    <title>Como gerar uma tabela de cores seguras para a web</title>
+  </head>
+
+  <body>
+
+
+    <?php 
+$saida = "";
+$saida = $saida . "<table
+width='750' 
+border='1' 
+cellspacing='0' 
+cellpadding='3'
+bordercolor='000000'>";
+
+for ($G =0; $G <= 255; $G = $G + 51){
+  for ($B =0; $B <=255; $B = $B + 51){
+    $saida = $saida . "<tr>";
+    for ($R = 0; $R <= 255; $R = $R + 51){
+      $saida = $saida . "<td style='background-color:rgb($R, $G, $B)'
+align='Center'><fontface='arial'
+size='4'>$R, $G, $B</font></td>";
+
+    }
+    $saida = $saida . "</tr>";
+  }
+}
+$saida = $saida . "</table>";
+?>
+
+    <h2>Tabela de cores seguras para a web</h2>
+    <?php print $saida; ?>
+  </body>
+</html>
